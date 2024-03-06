bc836217fd337d6ef6a53db2f778a0b7cd147093
akko-maker <158386515+akko-maker@users.noreply.github.com>
2024-03-06 11:04:11 +0900

    Add files via upload

---
+++ challenge-timer/js/main.js
@@ -0,0 +1,4 @@
+'use strict'
+// const out_time = document.getElementById('out_time');
+// const in_time = document.getElementById('in_time');
+// const control = document.getElementById('control');
